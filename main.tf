548b61869de63461d44119e2daa6b37c742f91a1
Ramanjulu321 <ramanjulu7890@gmail.com>
2024-06-14 16:48:36 +0530

    jenkins-practice

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ module "jenkins" {
   name = "jenkins-tf"
 
   instance_type          = "t3.small"
-  vpc_security_group_ids = ["sg-0c9e01998c14c4c22"] #replace your SG
-  subnet_id = "	subnet-03268e53152c328c8" #replace your Subnet
+  vpc_security_group_ids = ["sg-0fea5e49e962e81c9"] #replace your SG
+  subnet_id = "subnet-0ea509ad4cba242d7" #replace your Subnet
   ami = data.aws_ami.ami_info.id
   user_data = file("jenkins.sh")
   tags = {
@@ -19,9 +19,9 @@ module "jenkins_agent" {
   name = "jenkins-agent"
 
   instance_type          = "t3.small"
-  vpc_security_group_ids = ["sg-0c9e01998c14c4c22"]
+  vpc_security_group_ids = ["sg-0fea5e49e962e81c9"]
   # convert StringList to list and get first element
-  subnet_id = "subnet-03268e53152c328c8"
+  subnet_id = "subnet-0ea509ad4cba242d7"
   ami = data.aws_ami.ami_info.id
   user_data = file("jenkins-agent.sh")
   tags = {
@@ -29,16 +29,31 @@ module "jenkins_agent" {
   }
 }
 
+resource "aws_key_pair" "tools" {
+  key_name   = "tools"
+  # you can paste the public key directly like this
+  #public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL6ONJth+DzeXbU3oGATxjVmoRjPepdl7sBuPzzQT2Nc sivak@BOOK-I6CR3LQ85Q"
+  public_key = file("~/.ssh/tools.pub")
+  # ~ means windows home directory
+}
+
 module "nexus" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
   name = "nexus"
 
-  instance_type          = "t3.small"
-  vpc_security_group_ids = ["sg-0c9e01998c14c4c22"]
+  instance_type          = "t3.medium"
+  vpc_security_group_ids = ["sg-0fea5e49e962e81c9"]
   # convert StringList to list and get first element
-  subnet_id = "	subnet-03268e53152c328c8"
+  subnet_id = "subnet-0ea509ad4cba242d7"
   ami = data.aws_ami.nexus_ami_info.id
+  key_name = aws_key_pair.tools.key_name
+  root_block_device = [
+    {
+      volume_type = "gp3"
+      volume_size = 30
+    }
+  ]
   tags = {
     Name = "nexus"
   }
@@ -58,6 +73,7 @@ module "records" {
       records = [
         module.jenkins.public_ip
       ]
+      allow_overwrite = true
     },
     {
       name    = "jenkins-agent"
@@ -66,14 +82,17 @@ module "records" {
       records = [
         module.jenkins_agent.private_ip
       ]
+      allow_overwrite = true
     },
     {
       name    = "nexus"
       type    = "A"
       ttl     = 1
+      allow_overwrite = true
       records = [
         module.nexus.private_ip
       ]
+      allow_overwrite = true
     }
   ]
 
